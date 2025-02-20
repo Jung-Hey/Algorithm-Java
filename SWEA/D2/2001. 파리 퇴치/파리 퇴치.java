13e1f89bbf473968b57bcb3a357d1d72300c39a4
Jung-Hey <74144209+Jung-Hey@users.noreply.github.com>
2025-02-20 14:17:28 +0900

    [D2] Title: 파리 퇴치, Time: 150 ms, Memory: 32,512 KB -BaekjoonHub

--- SWEA/D2/2001. 파리 퇴치/파리 퇴치.java
+++ SWEA/D2/2001. 파리 퇴치/파리 퇴치.java
@@ -5,6 +5,12 @@ import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
 
+/**
+ * 32,896 kb
+ * 162 ms
+ * 2차원 누적합 배열을 이용해 이중 반복문을 돌며 m*m 크기만큼에 합에 해당하는 값을 
+ * 최대값 갱신을 반복
+ */
 class Solution
 {
     static int[][] arr;
@@ -15,7 +21,11 @@ class Solution
         // 누적합을 저장해 놓기 위한 배열에 저장
         for(int i=1; i<=n; i++) {
             for(int j=1; j<=n; j++) {
-                sumArr[i][j]= arr[i-1][j-1]+ sumArr[i-1][j] + sumArr[i][j-1] - sumArr[i-1][j-1] ;
+            	/*
+            	 *   누적합[i][j] = 
+            	 *   	기존배열[i-1][j-1] (원래 배열에 해당하는 더할 값) + (누적합배열 x축 -1 + y축 -1 값 ) - sumArr[i-1][j-1](겹치는 부분)
+            	 */
+                sumArr[i][j]= arr[i-1][j-1] + sumArr[i-1][j] + sumArr[i][j-1] - sumArr[i-1][j-1] ;
             }
         }
         //만들어진 누적합으로 m에 따른 계산
@@ -40,15 +50,13 @@ class Solution
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			n = Integer.parseInt(st.nextToken());
             m = Integer.parseInt(st.nextToken());
-            int answer=0;
             arr = new int[n][n];
              // 누적합 배열
         	sumArr = new int[n + 1][n + 1];
             for(int i=0;i<n;i++){
                arr[i] = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
             }
-            
-          	//누적합
+          	// solve
             sb.append("#").append(t).append(" ").append(solveMax()).append("\n");
 		}
 		System.out.println(sb);
