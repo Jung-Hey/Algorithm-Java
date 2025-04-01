d6838e4d65f8c94f7d8882652a332d10882cce79
Jung-Hey <74144209+Jung-Hey@users.noreply.github.com>
2025-04-01 16:49:35 +0900

    [Unrated] Title: [모의 SW 역량테스트] 수영장, Time: 153 ms, Memory: 33,152 KB -BaekjoonHub

--- SWEA/Unrated/1952. ［모의 SW 역량테스트］ 수영장/［모의 SW 역량테스트］ 수영장.java
+++ SWEA/Unrated/1952. ［모의 SW 역량테스트］ 수영장/［모의 SW 역량테스트］ 수영장.java
@@ -5,10 +5,10 @@ import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Queue;
 import java.util.Set;
 import java.util.StringTokenizer;
+import java.util.stream.IntStream;
 
 /**
  *  kb
@@ -16,54 +16,37 @@ import java.util.StringTokenizer;
  */
 public class Solution {
 	static StringBuilder sb;
-	static List<Integer> useList;
-	static int answer;
-	static int[]  price;
+
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));	
 		int tc = Integer.parseInt(br.readLine()), t=0;
 		sb = new StringBuilder();
 		while(t++<tc) {
-			
-			price = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-			int [] m = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+			int[] price = Arrays.stream(br.readLine().split(" "))
+				        		.mapToInt(Integer::parseInt).toArray();		
+			int [] month = IntStream.concat(IntStream.of(0),
+			        Arrays.stream(br.readLine().split(" "))
+	        		.mapToInt(Integer::parseInt)).toArray();
 			// 가장 높은 요금
-			answer = Arrays.stream(price).max().getAsInt();
-			//answer = Math.max(price[3], price[2]*4);
-			
-			useList = new ArrayList<>();
-			for(int day : m) {
-				if(day > 0) useList.add(day);
+			int answer = Arrays.stream(price).max().getAsInt();
+			// dy :  dy[x]는 0 ~ x월까지의 최소 수영장 이용 가격 
+			int [] dy = new int[12+1];
+			for (int i = 1; i <= 12; i++) {
+				// 1일권과 1개월 권 계산의 비교 
+				dy[i] = Math.min(dy[i-1] + month[i]*price[0], dy[i-1] + price[1]);
+				// 3개월 권의 비교
+				if(i >= 3) {
+					dy[i] = Math.min(dy[i] , dy[i-3]+price[2]);
+				}
 			}
-
-			if(useList.size() > 0) dfs(0,0);
-			else answer=0;
+			answer = Math.min(answer, dy[12]);
 			sb.append("#").append(t).append(" ").append(answer).append("\n");
 		}
-		
 		//output
 		System.out.println(sb);
 	
 		
 	}
-	
-
-	private static void dfs(int l, int sum) {
-		if(sum > answer) return;
-		if(l >= useList.size()) {
-			answer = Math.min(answer, sum);
-		}
-		else {
-			// 1일치로 계산
-			dfs(l+1, sum+price[0]*useList.get(l));
-			// 1달로 계산
-			dfs(l+1, sum+price[1]);
-			// 3달로 계산
-			dfs(l+3, sum+price[2]);
-		}
-	}
-
-
 
 
 }
