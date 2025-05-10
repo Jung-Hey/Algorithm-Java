7bfdbdb5d59360e9a821f2bc2774ecb3f69e356a
Jung-Hey <74144209+Jung-Hey@users.noreply.github.com>
2025-05-10 20:48:00 +0900

    [Gold I] Title: 구간 합 구하기, Time: 464 ms, Memory: 109112 KB -BaekjoonHub

--- 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.java
+++ 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.java
@@ -14,27 +14,30 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        K = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken()); // 수의 변경 횟수
+        K = Integer.parseInt(st.nextToken()); // 구간합을 구하는 횟수
 
         // 트리 높이, 크기, leaf 시작 인덱스 계산
         treeHeight = 0;
         int length = N;
+        // 세그머트 트리를 배열로 나타내기위한 2의 k 승이 > N 보다 커야하는 조건
         while (length != 0) {
             length /= 2;
             treeHeight++;
         }
 
         treeSize = (int) Math.pow(2, treeHeight + 1);   // 전체 트리 노드 개수
-        leftNodeStartIndex = treeSize / 2;              // 리프 노드 시작 인덱스
+        leftNodeStartIndex = treeSize / 2; // 리프 노드 시작 인덱스
         tree = new long[treeSize];
 
         // leaf 영역에만 값 입력
         for (int i = leftNodeStartIndex; i < leftNodeStartIndex + N; i++) {
             tree[i] = Long.parseLong(br.readLine());
         }
+        // (여기까지 세그먼트 트리 배열의 선언과 리프노트를 채운거임)
+        
 
-        // 부모 노드 채우
+        // 부모 노드 채우기 -> setTree-1 인자는 세그먼트 트리 배열의 오른쪽에서부터 구간합을 채워 넣기 위한 시작점
         setTree(treeSize - 1);
 
         StringBuilder sb = new StringBuilder();
@@ -48,33 +51,16 @@ public class Main {
                 int index = Integer.parseInt(st.nextToken());
                 long newValue = Long.parseLong(st.nextToken());
                 int treeIndex = leftNodeStartIndex + index - 1; // 입력값 → tree 배열 index 변환
+                // 부모 노드로만 이동해가며 업데이트 (log n)
+                changeVal(treeIndex, newValue);
 
-                long diff = newValue - tree[treeIndex];
-                tree[treeIndex] = newValue;
-
-                // 부모로만 올라가며 업데이트
-                while (treeIndex > 1) {
-                    treeIndex /= 2;
-                    tree[treeIndex] = tree[treeIndex * 2] + tree[treeIndex * 2 + 1];
-                }
             } else if (command == 2) { // 구간 합
                 int left = Integer.parseInt(st.nextToken());
                 int right = Integer.parseInt(st.nextToken());
 
-                //  구간을 leaf 인덱스로 변환
                 int leftIndex = leftNodeStartIndex + left - 1;
                 int rightIndex = leftNodeStartIndex + right - 1;
-                long sum = 0;
-
-                // 구간합 방식
-                while (leftIndex <= rightIndex) {
-                    if (leftIndex % 2 == 1) sum += tree[leftIndex++];  // 왼쪽이 오른쪽 자식일 경우
-                    if (rightIndex % 2 == 0) sum += tree[rightIndex--]; // 오른쪽이 왼쪽 자식일 경우
-                    leftIndex /= 2;
-                    rightIndex /= 2;
-                }
-
-                sb.append(sum).append("\n");
+                sb.append(getSum(leftIndex, rightIndex)).append("\n");
             }
         }
 
@@ -83,9 +69,37 @@ public class Main {
 
     //  setTree() 함수 (부모로 거슬러 올라가며 sum)
     static void setTree(int i) {
-        while (i > 1) {
+        // 루트 노드까지 가겠다는 뜻
+        while (i != 1) {
             tree[i / 2] += tree[i];
             i--;
         }
+    }
+
+    // 업데이트
+    static void changeVal(int index, long val) {
+        long diff = val - tree[index];
+        while (index > 0) {
+            tree[index] = tree[index] + diff;
+            index = index / 2;
+        }
+    }
+    // 구간 합 구하기
+    static long getSum(int s, int e) {
+        long partSum = 0;
+        // 시작점이 종료점과와 교차될 떄까지
+        while (s <= e) {
+            if (s % 2 == 1) { // 왼쪽이 오른쪽 자식인 경우
+                partSum += tree[s];
+                s++;
+            }
+            if (e % 2 == 0) { // 오른쪽이 왼쪽 자식인 경우
+                partSum += tree[e];
+                e--;
+            }
+            s /= 2;
+            e /= 2;
+        }
+        return partSum;
     }
 }
